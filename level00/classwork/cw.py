ef5f4c320b2333105b4fa5cbe955c4ba8b5f7ad2
AndriaShanava <jemalatyabladze2000@gmail.com>
2025-04-05 20:45:08 +0400

    commit

--- level00/classwork/cw.py
+++ level00/classwork/cw.py
@@ -1,10 +1,16 @@
-# age = int(input("Enter your age: "))
-# print(f"You are {age} years old.")
+age = int(input("Enter your age: "))
+print(f"You are {age} years old.")
+
+
+text = input("Enter the name: ")
+integer = int(input("Enter the number: "))
+print(text * integer)
+
 
 float_num = float(input("Enter the number: "))
 int_num = int(input("Enter the number: "))
 
-# მათემატიკური ოპერაციების შესრულება
+
 print(f"{float_num} + {int_num} = {float_num + int_num}")
 print(f"{float_num} - {int_num} = {float_num - int_num}")
 print(f"{float_num} * {int_num} = {float_num * int_num}")
