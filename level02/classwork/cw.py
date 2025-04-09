da71bb1d04a71b2f581e246c15cd48ac3d012132
AndriaShanava <jemalatyabladze2000@gmail.com>
2025-04-09 23:32:23 +0400

    commit

---
+++ level02/classwork/cw.py
@@ -0,0 +1,5 @@
+number = int(input("შეიყვანეთ რიცხვი: "))
+
+number_str = str(number)
+
+print(f"ახალი ცვლადი არის: {number_str}, და მისი ტიპი არის: {type(number_str)}")
---
+++ level02/classwork/cw3.py
@@ -0,0 +1,3 @@
+False or True and False and (True or 5 > 5)
+
+#ეს კოდი გამოიტანს False, რადგან 5 > 5 არის False, და True or False არის True, მაგრამ True and False არის False. და საბოლოოდ, False and True არის False.
