c4074cfe47d779232f5ef64ff8ebafe37121b20d
AndriaShanava <jemalatyabladze2000@gmail.com>
2025-04-09 21:00:15 +0400

    commit

---
+++ level01/classwork/classwork.py
@@ -0,0 +1,17 @@
+# first_name = "ანდრია"
+# favorite_number = 82
+# lives_in_city = "თბილისი"
+# is_student = True
+# favorite_color = "მწვანე"
+
+# print(first_name)
+# print(favorite_number)
+# print(lives_in_city)
+# print(is_student)
+# print(favorite_color)
+
+num = 5
+num2 = 10
+num3 = 15
+
+print(num,num2,num3)
---
+++ level01/classwork/cw.py
@@ -0,0 +1,6 @@
+name = "Andria"
+surname = "Shanava"
+age = 20
+height = 1.85
+
+print(f"შენ გვია {name}, შენი გვარია {surname}, შენ ხარ {age} წლის და შენი სიმაღლეა {height} მეტრი.")
