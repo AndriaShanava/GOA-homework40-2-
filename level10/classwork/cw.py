0c07a2b12979a866c26bfc532af4b363b6f79bf4
AndriaShanava <jemalatyabladze2000@gmail.com>
2025-05-10 20:38:46 +0400

    commit

---
+++ level10/classwork/cw.py
@@ -0,0 +1,16 @@
+#3)შექმენით სია სადაც გექნებათ მოთავსებული ყველა მონაცემთა ტიპის ელემენტი,
+#თქვენი დავალებაა ამ სიიდან ამოშალოთ მესამე და ბოლო ინდექსზე მდგომი ელემენტები,გამოიყენეთ pop() ფუნქცია
+data_types_list = [42, "hello", 3.14, True]
+data_types_list.pop(2)  
+data_types_list.pop(-1)  
+print(data_types_list)
+
+# 4)შექმენით სია --> ["giorgi" , "lasha", "beqa" , "irakli" , 45] თქვენი დავალებაა ამ სიიდან ამოშალოთ "lasha" და ასევე
+#ამოშალოთ რიცხვი 45,
+#გამოიყენეთ remove() ფუნქცია
+names_and_number = ["giorgi", "lasha", "beqa", "irakli", 45]
+names_and_number.remove("lasha")
+names_and_number.remove(45)
+print("Updated list after Task 4:", names_and_number)
+
+#pop() ფუნქცია ამოიღებს ელემენტს ინდექსით, ხოლო remove() ფუნქცია ამოიღებს ელემენტს მნიშვნელობით.
---
+++ level10/homework/homework.py
@@ -0,0 +1,83 @@
+# 1) შექმენით სია, სადაც მოცემული იქნება ყველა მონაცემთა ტიპის ელემენტები და დაამატეთ ელემენტი append ფუნქციის გამოყენებით
+data_types_list = [42, 3.14, "Hello", True]
+data_types_list.append("New Element")
+print(data_types_list)
+
+# მაგალითი 1
+example_list1 = [1, 2, 3]
+example_list1.append(4)
+print(example_list1)
+
+# მაგალითი 2
+example_list2 = ["a", "b", "c"]
+example_list2.append("d")
+print(example_list2)
+
+# 2) დაამატეთ ელემენტები კონკრეტულ ინდექსებზე insert ფუნქციის გამოყენებით
+data_types_list = [42, 3.14, "Hello", True, None, [1, 2, 3], {"key": "value"}, (4, 5)]
+data_types_list.insert(3, "Inserted at 3")
+data_types_list.insert(5, "Inserted at 5")
+data_types_list.insert(7, "Inserted at 7")
+print(data_types_list)
+
+# მაგალითი 1
+example_list1 = [1, 2, 3]
+example_list1.insert(1, "Inserted")
+print(example_list1)
+
+# მაგალითი 2
+example_list2 = ["a", "b", "c"]
+example_list2.insert(2, "Inserted")
+print(example_list2)
+
+# 3) განსხვავება insert და append ფუნქციებს შორის
+# append ამატებს ელემენტს სიის ბოლოში, ხოლო insert ამატებს ელემენტს კონკრეტულ ინდექსზე.
+
+# 4) ამოშალეთ ელემენტები კონკრეტულ ინდექსებზე pop ფუნქციის გამოყენებით
+data_types_list = [42, 3.14, "Hello", True, None, [1, 2, 3], {"key": "value"}, (4, 5)]
+data_types_list.pop(3)
+data_types_list.pop(2)
+data_types_list.pop(5)
+print(data_types_list)
+
+# მაგალითი 1
+example_list1 = [1, 2, 3, 4]
+example_list1.pop(1)
+print(example_list1)
+
+# მაგალითი 2
+example_list2 = ["a", "b", "c", "d"]
+example_list2.pop(2)
+print(example_list2)
+
+# 5) ამოშალეთ კონკრეტული ელემენტები remove ფუნქციის გამოყენებით
+data_types_list = [42, 3.14, "Hello", True, None, [1, 2, 3], {"key": "value"}, (4, 5)]
+data_types_list.remove(3.14)
+data_types_list.remove("Hello")
+data_types_list.remove(True)
+print(data_types_list)
+
+# მაგალითი 1
+example_list1 = [1, 2, 3, 4]
+example_list1.remove(2)
+print(example_list1)
+
+# მაგალითი 2
+example_list2 = ["a", "b", "c", "d"]
+example_list2.remove("c")
+print(example_list2)
+
+# 6) განსხვავება pop და remove ფუნქციებს შორის
+# pop ამოშლის ელემენტს ინდექსის მიხედვით და აბრუნებს მას, ხოლო remove ამოშლის კონკრეტულ მნიშვნელობას სიიდან.
+
+# 7) სიის გაერთიანება extend ფუნქციის გამოყენებით
+string_list = ["apple", "banana", "cherry"]
+integer_list = [1, 2, 3]
+string_list.extend(integer_list)
+print(string_list)
+
+float_list = [1.1, 2.2, 3.3]
+string_list.extend(float_list)
+print(string_list)
+
+# 8) გაიარეთ Sololearn-ის list ფუნქციების მოდული დამატებითი პრაქტიკისთვის.
