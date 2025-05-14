fa4403ce9a930dccf417781d918a8c6281d07bf7
AndriaShanava <jemalatyabladze2000@gmail.com>
2025-05-14 20:16:11 +0400

    commit

---
+++ level11/classwork/cw.py
@@ -0,0 +1,14 @@
+def multiply_numbers(a, b):
+    return a * b
+
+result1 = multiply_numbers(3, 5)
+print(result1)
+
+result2 = multiply_numbers(7, 8)
+print(result2)
+
+result3 = multiply_numbers(10, 15)
+print(result3)
+
+result4 = multiply_numbers(2, 9)
+print(result4)
---
+++ level11/classwork/cw1.py
@@ -0,0 +1,3 @@
+def sum_of_numbers(list1):
+    return sum(list1)
+print(sum_of_numbers([1, 2, 3, 4, 5]))
