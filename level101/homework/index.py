90ca82191295eea49f94dad65b35c241d1ed2fac
AndriaShanava <jemalatyabladze2000@gmail.com>
2026-04-15 20:46:41 +0400

    commit

---
+++ level101/homework/index.py
@@ -0,0 +1,14 @@
+#1)
+# def switcheroo(string):
+#     result = ''
+#     for letter in string:
+#         if letter == 'a':
+#             letter = 'b'
+#         elif letter == 'b':
+#             letter = 'a'
+#         result += letter
+#     return result
+
+
+#2)
+
