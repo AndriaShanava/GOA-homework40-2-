1f4f18e881e63cc6724862970b68be7065b363af
AndriaShanava <jemalatyabladze2000@gmail.com>
2026-04-25 18:16:21 +0400

    commit

---
+++ level104/classwork/index.js
@@ -0,0 +1,10 @@
+// console.log('Andria');
+// console.log(15);
+// console.log(true);
+// console.log(1.75)
+
+let name1 = 'nika'
+const name2 = 'lasha'
+var name3 = 'andria'
+
+console.log(name1 + ' ' + name2 + ' ' + name3);
---
+++ level104/homework/index.js
@@ -0,0 +1,35 @@
+// console.log() - ეს არის ფუნქცია, რომელიც გამოიყენება მონაცემების კონსოლში გამოსატანად.
+console.log('Andria')
+console.log(15)
+console.log(1.75)
+// let, const, var - ეს არის ცვლადების დეკლარაციის მეთოდები.
+// let - ეს არის ცვლადი, რომლის მნიშვნელობა შეიძლება შეიცვალოს.
+// let name1 = 'nika'
+// const - ეს არის ცვლადი, რომლის მნიშვნელობა არ შეიძლება შეიცვალოს.
+// const name2 = 'lasha'
+// var - ეს არის ცვლადი, რომელიც გამოიყენებოდა ძველ ვერსიებში.
+// var name3 = 'andria'
+
+// let x = 10
+// var name4 = 'andria'
+// const y = 1.75
+
+// console.log(x)
+// console.log(name4)
+// console.log(y)
+
+let x = 10
+let y = 1.75
+console.log(x + y) 
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+
+console.log(x % y)
+
+let firstName = 'Andria'
+let lastName = 'shanava'
+console.log(firstName + ' ' + lastName)
+
+let name1 = 'andria'
+console.log('My name is ' + name1)
